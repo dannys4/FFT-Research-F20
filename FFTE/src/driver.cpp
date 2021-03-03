a1a01de004367a3e4afd062cfe3c871ce1c791cd
dannys4 <dannys4@vt.edu>
2021-03-02 22:16:39 -0500

    Corrected example in driver, this works

--- FFTE/src/driver.cpp
+++ FFTE/src/driver.cpp
@@ -1,7 +1,6 @@
 #include "test.hpp"
 #include "Engine.hpp"
 #include <iostream>
-#include <vector>
 
 /**
  * Code Author: Danny Sharp
@@ -11,16 +10,14 @@ using namespace FFTE;
 // Main function. Used for calling different parts of the testing code
 int main() {
     const int n = 4*9*7;
-    FFTE::complex_vector<double, 4> input ();
+    FFTE::complex_vector<double, 4> input {};
     // FFTE::Complex<double, 4>* input2 = new FFTE::Complex<double, 4>[20];
     for(int i = 0; i < n; i++) {
-        Complex<double, 4> tmp {1.*i, 2.*i, 3.*i, 4.*i};
-        input.push_back(tmp);
-        std::cout << "tmp = " << tmp << ", input[i] = " << input[i] << "\n";
+        input.push_back(Complex<double, 4> {1.*i, 2.*i, 3.*i, 4.*i});
     }
     std::cout << "Output:\n";
-    auto output = fft<double, 4>(input, FFTE::Direction::forward);
-    // for(int i = 0; i < n; i++) {
-    //     std::cout << output[i] << "\n";
-    // }
+    auto output = fft<double, 4>(input, FFTE::Direction::inverse);
+    for(int i = 0; i < n; i++) {
+        std::cout << output[i]/((double) n) << "\n";
+    }
 }
